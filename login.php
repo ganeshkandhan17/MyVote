94ac961689f65927ffbdfdcd9d36fcc101a22cda
Ganesh Kandhan V M <87404827+ganeshkandhan17@users.noreply.github.com>
2023-04-27 18:57:13 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+//connection form and mysql server
+
+$con=mysqli_connect("localhost","root","","home");
+
+//user name password receive login page
+
+$usertrim= trim($_POST['username']);
+
+$userstrip=stripcslashes($usertrim);
+$finaluser = htmlspecialchars($userstrip);
+
+// password 
+
+$passtrim= trim($_POST['password']);
+
+$passstrip=stripcslashes($passtrim);
+$finalpass = htmlspecialchars($passstrip);
+
+//comparision user input and data base
+
+$sql = "SELECT * FROM register where username='$finaluser' AND password = '$finalpass'";
+
+//sql request excuted
+$result = mysqli_query($con,$sql);
+
+//if number of row isgreatervthan o there is username and passwword
+
+// match is found else is not found
+
+if(mysqli_num_rows($result)>=1)
+{
+  //user is stored to session and forward next page
+  $_SESSION["myuser"]=$finaluser;
+  header("Location:MY VOTE.html");
+}else{
+  //error is shown
+  $_SESSION["error"]="you are not valid user";
+    echo"user name or password are not valid";
+}
+?>
---
+++ register.php
@@ -0,0 +1,62 @@
+<?php
+
+$username = $_POST['username'];
+$email  = $_POST['email'];
+$password = $_POST['password'];
+$confirmpassword = $_POST['confirmpassword'];
+
+
+
+
+if (!empty($username) || !empty($email) || !empty($password) || !empty($confirmpassword) )
+{
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "home";
+
+
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+
+}
+else{
+  $SELECT = "SELECT email From register Where email = ? Limit 1";
+  $INSERT = "INSERT Into register (username , email ,password, confirmpassword )values(?,?,?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssss", $username,$email,$password,$confirmpassword);
+      $stmt->execute();
+      header("Location: sign in.html");
+      exit;
+     } else {
+      function function_alert($message){
+      echo "<script>alert('$message');</script>";
+    }
+    function_alert("email already exists");
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
