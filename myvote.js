03bf504ab51b9580fbc8c11da3b5b590b5ff8f29
GANESH KANDHAN V M <ganeshkandhan@gmail.com>
2023-06-21 19:57:24 +0530

    pool creation added frontend

---
+++ myvote.js
@@ -0,0 +1,32 @@
+window.onload=function(){
+    let inn=document.getElementById("create");
+    inn.addEventListener("click",create)
+    function create(){
+        let in1=document.getElementById("input3");
+        let len=Number(in1.value);
+        for(let i=0;i<len;i++){
+        let intag=document.createElement("li");
+        let list=document.getElementById("list");
+        intag.innerHTML="<p id='label'> Pool - "+(i+1)+"</p><input id='poolin' type='text'>"
+        list.appendChild(intag);
+    }
+}
+    let del=document.getElementById("delet");
+    del.addEventListener("click",delet);
+    function delet(){
+        try{
+            let del=document.querySelector("#list li:last-child");
+            del.remove();
+        }
+        catch(error){
+            alert("All pools was deleted");
+        }
+    }
+    // let reset=document.getElementById("reset");
+    // reset.addEventListener("click",resetall);
+    // function resetall(){
+    //     let delet=document.querySelector("#list");
+    //     delet.remove()
+    // }
+    
+}
