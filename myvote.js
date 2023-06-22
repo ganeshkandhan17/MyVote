fbb56f4ad54dafc3dcf5391cdbdaeaf25984aba8
GANESH KANDHAN V M <ganeshkandhan@gmail.com>
2023-06-23 00:00:49 +0530

    my vote changes

--- myvote.js
+++ myvote.js
@@ -1,16 +1,38 @@
 window.onload=function(){
+    // idtansfer
+    if (typeof(Storage) !== "undefined"){
+        document.getElementById("id").innerHTML=localStorage.getItem("id");
+        }
+    // create 
     let inn=document.getElementById("create");
-    inn.addEventListener("click",create)
+    inn.addEventListener("click",create);
     function create(){
-        let in1=document.getElementById("input3");
-        let len=Number(in1.value);
-        for(let i=0;i<len;i++){
-        let intag=document.createElement("li");
-        let list=document.getElementById("list");
-        intag.innerHTML="<p id='label'> Pool - "+(i+1)+"</p><input name='pool"+(i+1)+"'id='poolin' type='text'>"
-        list.appendChild(intag);
-    }
+        try{
+            resetall();
+            let in1=document.getElementById("input3");
+            let len=Number(in1.value);
+            for(let i=0;i<len;i++){
+            let intag=document.createElement("li");
+            intag.id="liitem";
+            let list=document.getElementById("list");
+            intag.innerHTML="<p id='label'> Pool - "+(i+1)+"</p><input name='pool"+(i+1)+"'id='poolin' type='text'>"
+            list.appendChild(intag);
+            }
+        }
+        catch(error){
+            let in1=document.getElementById("input3");
+            let len=Number(in1.value);
+            for(let i=0;i<len;i++){
+            let intag=document.createElement("li");
+            intag.id="liitem";
+            let list=document.getElementById("list");
+            intag.innerHTML="<p id='label'> Pool - "+(i+1)+"</p><input name='pool"+(i+1)+"'id='poolin' type='text'>"
+            list.appendChild(intag);
+            }
+        }
 }
+    // -------------------------------------------- 
+    // Delet 
     let del=document.getElementById("delete");
     del.addEventListener("click",delet);
     function delet(){
@@ -22,12 +44,19 @@ window.onload=function(){
             alert("All pools was deleted");
         }
     }
-    // let reset=document.getElementById("reset");
-    // reset.addEventListener("click",resetall);
-    // function resetall(){
-    //     let delet=document.querySelector("#list");
-    //     delet.remove()
-    // }
+    // ---------------------- 
+    // reset all 
+    let reset=document.getElementById("reset");
+    reset.addEventListener("click",resetall);
+    function resetall(){
+        let in1=document.getElementById("input3");
+        let len=Number(in1.value);
+            let delet=document.querySelectorAll("#liitem");
+            for(let i=0;i<delet.length;i++){
+                delet[i].remove();
+            }
+    }
+    // --------------------
     let resultpage1=document.getElementById("result");
     resultpage1.addEventListener("click",resultpage);
     function resultpage(){
