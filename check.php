7c883c0607f044bb5e99a1e9c423a28088643a0e
purushothamanjerry <97541749+purushothamanjerry@users.noreply.github.com>
2023-06-26 11:48:08 +0530

    Add files via upload
    add alert msg to redirect

--- check.php
+++ check.php
@@ -1,26 +1,20 @@
 <?php
-
-$con=new mysqli('localhost','root','','vote');
-if($con->connect_errno)
-{
+$con = new mysqli('localhost', 'root', '', 'vote');
+if ($con->connect_errno) {
     echo $con->connect_error;
     die();
-
 }
-$gmail=$_GET['email'];
 
-//echo "$gmail";
+$gmail = $_GET['email'];
 
 $sql = "SELECT password FROM register WHERE email='$gmail'";
 $result = $con->query($sql);
 if ($result->num_rows > 0) {
-   //output data of each row
-  while($row = $result->fetch_assoc()) {
-   //echo  $row["password"]. "<br>";
-    
-$min=$row['password'];
-//echo $min;
-}}
+    while ($row = $result->fetch_assoc()) {
+        $min = $row['password'];
+    }
+}
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -28,37 +22,30 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
-    // code...
-
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'myrightsmyvote@gmail.com';                     //SMTP username
-    $mail->Password   = 'xpyylhtqpcutmfhg';                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'myrightsmyvote@gmail.com';
+    $mail->Password = 'xpyylhtqpcutmfhg';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-    //Recipients
     $mail->setFrom('myrightsmyvote@gmail.com', 'myvote');
-    $mail->addAddress($_GET['email']);     //Add a recipient
-   
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'your Password is';
-    $mail->Body    = $min;
-    $content="my name is billa";
+    $mail->addAddress($_GET['email']);
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Forgot password';
+    $mail->Body =  $min . ' Use this password to login and change the password ' ;
+    $content = "my name is billa";
     $mail->AltBody = 'hello world';
 
     $mail->send();
-    echo '<script>alert("your Password has sent to your mail .use to login,after change the password")</script>';
+    echo '<script>alert("Your password has been sent to your email. Please use it to log in and change the password."); window.location.href = "SignIn.html";</script>';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
 ?>
--- newpas.php
+++ newpas.php
@@ -1,33 +1,63 @@
-<?php 
+<?php
 
-$con=new mysqli('localhost','root','','vote');
-if($con->connect_errno)
-{
+$con = new mysqli('localhost', 'root', '', 'vote');
+if ($con->connect_errno) {
     echo $con->connect_error;
     die();
-
 }
 
-$nnumber=$_POST['number'];
+$nnumber = $_POST['number'];
+$opassword = $_POST['oldpassword'];
+$password = $_POST['newpassword'];
 
-$opassword=$_POST['oldpassword'];
-$password=$_POST['newpassword'];
+// Prepare the SQL query to check if the mobile number exists in the database
+$query = "SELECT COUNT(*) as count FROM register WHERE number = '$nnumber'";
+$resul = mysqli_query($con, $query);
 
-///echo $nnumber;
-//echo $opassword;
+if ($resul) {
+    $row = mysqli_fetch_assoc($resul);
+    $count = $row['count'];
 
-//echo $password;
+    if ($count > 0) {
+        // Mobile number exists in the database, check if the given password matches
+        $quer = "SELECT * FROM register WHERE number = '$nnumber' AND password = '$opassword'";
+        $res = mysqli_query($con, $quer);
 
+        if ($res) {
+            $count = mysqli_num_rows($res);
 
-$sql = "UPDATE register SET confirmpassword = '$password', password = '$password' WHERE number='$nnumber' AND confirmpassword='$opassword'";
+            if ($count > 0) {
+                // Mobile number and old password match, update the password
+                $sql = "UPDATE register SET confirmpassword = '$password',password = '$password'   WHERE number='$nnumber' AND password='$opassword'";
+                $result = $con->query($sql);
 
-if($result = $con->query($sql))
-{
-	echo  'password changed successfully'. "<br>";
-	
+                if ($result) {
+                    // Password update successful
+                    echo '<script>alert("Password changed successfully."); window.location.href = "SignIn.html";</script>';
 
-}else{
-	echo 'password not changed';
+                } else {
+                    // Error updating the password
+                    echo "Error: " . mysqli_error($con);
+                }
+            } else {
+                // Mobile number and old password do not match
+            
+
+    echo '<script>alert("Mobile number and password do not match."); window.location.href = "changepass.html";</script>';   
+            }
+        } else {
+            // Error executing the query
+            echo "Error: " . mysqli_error($con);
+        }
+    } else {
+        // Mobile number does not exist in the database
+        echo '<script>alert("Mobile number not found in the database. Please enter a valid number."); window.location.href = "changepass.html";</script>';   
+    }
+} else {
+    // Error executing the query
+    echo "Error: " . mysqli_error($con);
 }
 
- ?>
+// Close the database connection
+mysqli_close($con);
+?>
