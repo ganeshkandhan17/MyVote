4b0d7a8f4a8ccc621b24633146d87410bfa0ef45
purushothamanjerry <97541749+purushothamanjerry@users.noreply.github.com>
2023-06-30 14:39:30 +0530

    Add files via upload
    new change

--- login.php
+++ login.php
@@ -1,43 +1,56 @@
 <?php
-// session_start();
+$con = mysqli_connect("localhost", "root", "", "vote");
 
-//connection form and mysql server
-
-$con=mysqli_connect("localhost","root","","vote");
-
-//user name password receive login page
-
-$usertrim= trim($_POST['email']);
-
-$userstrip=stripcslashes($usertrim);
-$finaluser = htmlspecialchars($userstrip);
-
-// password 
-
-$passtrim= trim($_POST['password']);
-
-$passstrip=stripcslashes($passtrim);
-$finalpass = htmlspecialchars($passstrip);
-
-//comparision user input and data base
-
-$sql = "SELECT * FROM register where email='$finaluser' AND password = '$finalpass'";
-
-//sql request excuted
-$result = mysqli_query($con,$sql);
-
-//if number of row isgreatervthan o there is username and passwword
-
-// match is found else is not found
-
-if(mysqli_num_rows($result)>=1)
-{
-  //user is stored to session and forward next page
-  $_SESSION["myuser"]=$finaluser;
-  header("Location:myvote.html");
-}else{
-  //error is shown
-  $_SESSION["error"]="you are not valid user";
-    echo '<script>alert("user name or password are not valid"); window.location.href = "SignIn.html";</script>';
+// Check if the connection was successful
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
 }
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Prepare the query to check email and password in the 'register' table
+$query = "SELECT * FROM register WHERE email = '$email' AND password = '$password'";
+$result = mysqli_query($con, $query);
+
+// Check if the query execution was successful
+if ($result) {
+    // Check if a row was found with the provided email and password
+    if (mysqli_num_rows($result) > 0) {
+        // The email and password are matching in the 'register' table
+
+        // Check if the email already exists in the 'user' table
+        $checkQuery = "SELECT * FROM user WHERE email = '$email'";
+        $checkResult = mysqli_query($con, $checkQuery);
+
+        // Check if the query execution was successful
+        if ($checkResult) {
+            // Check if a row was found with the provided email in the 'user' table
+            if (mysqli_num_rows($checkResult) > 0) {
+                // The email already exists in the 'user' table
+                header("Location: myvote.php");
+                exit();
+            } else {
+                // The email does not exist in the 'user' table
+                header("Location: myvote.html");
+                exit();
+            }
+        } else {
+            // Error executing the query for checking email in the 'user' table
+            echo "Error: " . mysqli_error($con);
+            exit();
+        }
+    } else {
+        // No matching email and password found in the 'register' table
+        // Handle the appropriate action or show an error message
+    }
+} else {
+    // Error executing the query for checking email and password in the 'register' table
+    echo "Error: " . mysqli_error($con);
+    exit();
+}
+
+// Close the database connection
+mysqli_close($con);
 ?>
