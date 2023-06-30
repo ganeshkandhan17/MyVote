21a00f02cae733528e9480de797664ee15656f2d
purushothamanjerry <97541749+purushothamanjerry@users.noreply.github.com>
2023-06-30 19:41:11 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -19,29 +19,11 @@ if ($result) {
     // Check if a row was found with the provided email and password
     if (mysqli_num_rows($result) > 0) {
         // The email and password are matching in the 'register' table
+        header("Location: myvote.html");
 
-        // Check if the email already exists in the 'user' table
-        $checkQuery = "SELECT * FROM user WHERE email = '$email'";
-        $checkResult = mysqli_query($con, $checkQuery);
-
-        // Check if the query execution was successful
-        if ($checkResult) {
-            // Check if a row was found with the provided email in the 'user' table
-            if (mysqli_num_rows($checkResult) > 0) {
-                // The email already exists in the 'user' table
-                header("Location: myvote.php");
-                exit();
-            } else {
-                // The email does not exist in the 'user' table
-                header("Location: myvote.html");
-                exit();
-            }
-        } else {
-            // Error executing the query for checking email in the 'user' table
-            echo "Error: " . mysqli_error($con);
-            exit();
-        }
+        
     } else {
+        echo '<script>alert("Invalid user"); window.location.href = "SignIn.html";</script>';
         // No matching email and password found in the 'register' table
         // Handle the appropriate action or show an error message
     }
